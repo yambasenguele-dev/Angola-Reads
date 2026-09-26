9b5485a0eb65f198a159cb924a850b385239ed0f
Yamba Gabriel Adolfo Senguele <yambasenguele@gmail.com>
2026-09-26 08:04:25 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -74,6 +74,9 @@ export const metadata: Metadata = {
   icons: {
     icon: "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>📚</text></svg>",
   },
+  verification: {
+    google: 'eY59hiTOzrl8LPhWgl9-evJwBYVsXBQJR4o9TGSvNX0',
+  },
 }
 
 export default function RaizLayout({
